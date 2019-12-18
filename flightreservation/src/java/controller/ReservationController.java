540ef51b7f07be9216705c3f5306f723085a5172
snehikav <59025211+snehikav@users.noreply.github.com>
2019-12-18 11:24:45 -0600

    Add files via upload

---
+++ flightreservation/src/java/controller/ReservationController.java
@@ -0,0 +1,50 @@
+package com.snehika.flightreservation.controllers;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import com.snehika.flightreservation.dto.ReservationRequest;
+import com.snehika.flightreservation.entities.Flight;
+import com.snehika.flightreservation.entities.Reservation;
+import com.snehika.flightreservation.repos.FlightRepository;
+import com.snehika.flightreservation.services.ReservationService;
+
+@Controller
+public class ReservationController {
+
+	@Autowired
+	FlightRepository flightRepository;
+
+	@Autowired
+	ReservationService reservationService;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ReservationController.class);
+
+	@RequestMapping("/showCompleteReservation")
+	public String showCompleteReservation(@RequestParam("flightId") Long flightId, ModelMap modelMap) {
+		LOGGER.info("showCompleteReservation() invoked with the Flight Id: " + flightId);
+		Flight flight = flightRepository.findById(flightId).get();
+		modelMap.addAttribute("flight", flight);
+		LOGGER.info("Flight is:" + flight);
+		return "completeReservation";
+
+	}
+	@RequestMapping(value = "/completeReservation", method={ RequestMethod.GET, RequestMethod.POST })
+	public String completeReservation(ReservationRequest request, ModelMap modelMap) {
+		LOGGER.info("completeReservation()  " + request);
+
+		Reservation reservation = reservationService.bookFlight(request);
+		modelMap.addAttribute("msg", "Reservation created successfully and the id is " + reservation.getId());
+		return "reservationConfirmation";
+
+	}
+
+}
